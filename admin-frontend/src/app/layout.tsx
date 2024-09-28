b342be661ca5ba6cb051b51f9ceab2251e0b472e
shahanas7210 <shahanas7210@gmail.com>
2024-09-28 22:22:02 +0530

    Feat: Remove unwanted code and add provider on layout

--- admin-frontend/src/app/layout.tsx
+++ admin-frontend/src/app/layout.tsx
@@ -5,16 +5,14 @@ import "@/css/satoshi.css";
 import "@/css/style.css";
 import React, { useEffect, useState } from "react";
 import Loader from "@/components/common/Loader";
+import Provider from "./provider";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   const [loading, setLoading] = useState<boolean>(true);
-
-  // const pathname = usePathname();
 
   useEffect(() => {
     setTimeout(() => setLoading(false), 1000);
@@ -24,7 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body suppressHydrationWarning={true}>
         <div className="dark:bg-boxdark-2 dark:text-bodydark">
-          {loading ? <Loader /> : children}
+          <Provider>{loading ? <Loader /> : children}</Provider>
         </div>
       </body>
     </html>
