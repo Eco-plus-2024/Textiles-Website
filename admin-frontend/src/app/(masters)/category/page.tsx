18075fa696a9b1f4e811b4462555b184f8d61cb3
shahanas7210 <shahanas7210@gmail.com>
2024-09-28 22:23:14 +0530

    Fix:  Remove unwanted import

--- admin-frontend/src/app/(masters)/category/page.tsx
+++ admin-frontend/src/app/(masters)/category/page.tsx
@@ -1,6 +1,5 @@
 import CategoryForm from "@/components/Category/CategoryForm";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
-import React from "react";
 
 const page = () => {
   return (
